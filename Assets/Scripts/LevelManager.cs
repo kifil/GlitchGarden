c37ac824d2a40024bc3148c27fbef411b0a8f9cd
Daniel Wilson <wilsondr16@gmail.com>
2016-01-24 13:03:09 -0600

    Initial project setup Imported old project assets Added new project assets Added splash screen

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public bool autoLoadNextLevel = false;
+	public float loadNextLevelAfter;
+
+	void Start(){
+		if(autoLoadNextLevel){
+			Invoke ("LoadNextLevel",loadNextLevelAfter);
+		}
+	}
+
+	public void LoadLevel(string name){
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest(){
+		Debug.Log("quit requested");
+		Application.Quit();
+	}
+	
+	//Loads next level by index
+	public void LoadNextLevel(){
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	
+	static MusicPlayer instance = null;
+	
+	public AudioClip splashClip;
+	public AudioClip startClip;
+	public AudioClip gameClip;
+	
+	private AudioSource myAudioSource;
+	
+	void Awake (){
+		if(instance != null){
+			Destroy(gameObject);
+		}
+		//else this is the first music player
+		else{
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+			myAudioSource = GetComponent<AudioSource>();
+			//default clip
+			myAudioSource.clip = splashClip;
+			myAudioSource.loop = false;
+			myAudioSource.Play();
+		}
+	}
+	
+	//build in function that is called when a level is changed
+	void OnLevelWasLoaded(int level){
+	//TODO: clean this whole file up
+		myAudioSource.loop = true;
+		if(myAudioSource){
+			myAudioSource.Stop();
+		
+//			if(level == 0){
+//				myAudioSource.clip = startClip;
+//			}
+			if(level == 1){
+				myAudioSource.clip = gameClip;
+			}
+			else if(level == 2){
+				myAudioSource.clip = gameClip;
+			}
+		
+			myAudioSource.Play();
+		}
+	}
+
+
+}
