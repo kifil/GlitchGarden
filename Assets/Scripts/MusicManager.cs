aed6b7084e3ceefa71ca4e982eb15b3585899c18
Daniel Wilson <wilsondr16@gmail.com>
2016-01-24 22:18:33 -0600

    Set up project scenes Started setting up options and preferences

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicArray;
+	
+	private AudioSource myAudioSource;
+	// Use this for initialization
+	void Start () {
+		GameObject.DontDestroyOnLoad(gameObject);
+		myAudioSource = GetComponent<AudioSource>();
+	}
+	
+	void OnLevelWasLoaded(int level){
+		AudioClip thisLevelAudio = levelMusicArray[level];
+		
+		if(thisLevelAudio){
+			myAudioSource.clip = thisLevelAudio;
+			myAudioSource.Play();
+		}
+		else{
+			myAudioSource.Stop();
+		}
+	}
+	
+}
---
+++ Assets/Scripts/TransitionPanel.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TransitionPanel : MonoBehaviour {
+
+	public float fadeInTime;
+
+	private Image myImage;
+	private float currentAlpha = 1;
+	// Use this for initialization
+	void Start () {
+		myImage = GetComponent<Image>();
+		myImage.color = new Color(0,0,0,currentAlpha);
+//		InvokeRepeating("FadeIn",0.0001f,.1f);
+	}
+	
+	void Update(){
+		if(Time.timeSinceLevelLoad < fadeInTime){
+			float fadeInPercent = Time.timeSinceLevelLoad / fadeInTime;
+			myImage.color = new Color(0,0,0,1 - fadeInPercent);
+		}
+		else{
+			gameObject.SetActive(false);
+		}
+	
+	}
+	
+//	void FadeIn(){
+//		currentAlpha-=.02f;
+//		myImage.color = new Color(0,0,0,currentAlpha);
+//		if(currentAlpha <=0){
+//			Destroy(this);
+//		}
+//	}
+}
